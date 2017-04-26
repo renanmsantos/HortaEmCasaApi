2cd7ee35d31886149569989e8285b5a965701d41
Renan Moreira Santos <renanmoreirasan@gmail.com>
2017-04-26 19:42:44 -0300

    Mudando estrutura do projeto

--- src/main/java/br/com/fatec/HortaemcasaApplication.java
+++ src/main/java/br/com/fatec/HortaemcasaApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HortaemcasaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HortaemcasaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HortaemcasaApplication.class, args);
+    }
 }
